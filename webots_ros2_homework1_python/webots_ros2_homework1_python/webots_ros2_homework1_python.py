9e831339a4a7f4080bcdc58a2c98983a2509af10
pbhattacharjee-repo <pbhattacharjee1@crimson.ua.edu>
2024-11-18 13:40:32 -0600

    Update webots_ros2_homework1_python.py sent_through_wsapp

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -1,16 +1,12 @@
 import rclpy
-# import the ROS2 python libraries
 from rclpy.node import Node
-# import the Twist module from geometry_msgs interface
 from geometry_msgs.msg import Twist
-# import the LaserScan module from sensor_msgs interface
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-# import Quality of Service library, to set the correct profile and reliability in order to read sensor data.
+from apriltag_msgs.msg import AprilTagDetectionArray
+from tf2_msgs.msg import TFMessage
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 import math
-
-
 
 LINEAR_VEL = 0.22
 STOP_DISTANCE = 0.2
@@ -19,14 +15,13 @@ LIDAR_AVOID_DISTANCE = 0.7
 SAFE_STOP_DISTANCE = STOP_DISTANCE + LIDAR_ERROR
 RIGHT_SIDE_INDEX = 270
 RIGHT_FRONT_INDEX = 210
-LEFT_FRONT_INDEX=150
-LEFT_SIDE_INDEX=90
+LEFT_FRONT_INDEX = 150
+LEFT_SIDE_INDEX = 90
 
 class RandomWalk(Node):
 
-    def __init__(self):
-        # Initialize the publisher
-        super().__init__('random_walk_node')
+    def _init_(self):
+        super()._init_('random_walk_node')
         self.scan_cleaned = []
         self.stall = False
         self.turtlebot_moving = False
@@ -41,115 +36,99 @@ class RandomWalk(Node):
             '/odom',
             self.listener_callback2,
             QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.apriltag_detection_sub = self.create_subscription(
+            AprilTagDetectionArray,
+            '/detections',
+            self.apriltag_callback,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))
+        
         self.laser_forward = 0
         self.odom_data = 0
-        timer_period = 0.5
-        self.pose_saved=''
+        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.pose_saved = ''
         self.cmd = Twist()
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-
+        self.apriltag_detected = False
+        self.apriltag_id = None
 
     def listener_callback1(self, msg1):
-        #self.get_logger().info('scan: "%s"' % msg1.ranges)
         scan = msg1.ranges
         self.scan_cleaned = []
-       
-        #self.get_logger().info('scan: "%s"' % scan)
-        # Assume 360 range measurements
         for reading in scan:
             if reading == float('Inf'):
                 self.scan_cleaned.append(3.5)
             elif math.isnan(reading):
                 self.scan_cleaned.append(0.0)
             else:
-            	self.scan_cleaned.append(reading)
-
-
+                self.scan_cleaned.append(reading)
 
     def listener_callback2(self, msg2):
         position = msg2.pose.pose.position
-        orientation = msg2.pose.pose.orientation
-        (posx, posy, posz) = (position.x, position.y, position.z)
-        (qx, qy, qz, qw) = (orientation.x, orientation.y, orientation.z, orientation.w)
-        self.get_logger().info('self position: {},{},{}'.format(posx,posy,posz));
-        # similarly for twist message if you need
-        self.pose_saved=position
-        
-        #Example of how to identify a stall..need better tuned position deltas; wheels spin and example fast
-        #diffX = math.fabs(self.pose_saved.x- position.x)
-        #diffY = math.fabs(self.pose_saved.y - position.y)
-        #if (diffX < 0.0001 and diffY < 0.0001):
-           #self.stall = True
-        #else:
-           #self.stall = False
-           
-        return None
-        
+        self.pose_saved = position
+        self.get_logger().info('Self position: {},{},{}'.format(
+            position.x, position.y, position.z))
+
+    def apriltag_callback(self, msg):
+        """Callback for AprilTag detections."""
+        if not msg.detections:
+            self.get_logger().info('No AprilTag detected.')
+            return
+
+        for detection in msg.detections:
+            tag_id = detection.id
+            centre = detection.centre
+            corners = detection.corners
+
+            self.get_logger().info(
+                f'AprilTag Detected - ID: {tag_id}, Centre: [x: {centre.x}, y: {centre.y}]')
+            self.apriltag_detected = True
+            self.apriltag_id = tag_id
+
     def timer_callback(self):
-        if (len(self.scan_cleaned)==0):
-    	    self.turtlebot_moving = False
-    	    return
-    	    
-        #left_lidar_samples = self.scan_cleaned[LEFT_SIDE_INDEX:LEFT_FRONT_INDEX]
-        #right_lidar_samples = self.scan_cleaned[RIGHT_FRONT_INDEX:RIGHT_SIDE_INDEX]
-        #front_lidar_samples = self.scan_cleaned[LEFT_FRONT_INDEX:RIGHT_FRONT_INDEX]
+        if len(self.scan_cleaned) == 0:
+            self.turtlebot_moving = False
+            return
         
         left_lidar_min = min(self.scan_cleaned[LEFT_SIDE_INDEX:LEFT_FRONT_INDEX])
         right_lidar_min = min(self.scan_cleaned[RIGHT_FRONT_INDEX:RIGHT_SIDE_INDEX])
         front_lidar_min = min(self.scan_cleaned[LEFT_FRONT_INDEX:RIGHT_FRONT_INDEX])
 
-        #self.get_logger().info('left scan slice: "%s"'%  min(left_lidar_samples))
-        #self.get_logger().info('front scan slice: "%s"'%  min(front_lidar_samples))
-        #self.get_logger().info('right scan slice: "%s"'%  min(right_lidar_samples))
+        if self.apriltag_detected:
+            self.cmd.linear.x = 0.0
+            self.cmd.angular.z = 0.0
+            self.publisher_.publish(self.cmd)
+            self.get_logger().info(f'Stopping for detected AprilTag ID: {self.apriltag_id}')
+            self.apriltag_detected = False  # Reset detection for subsequent detections
+            return
 
         if front_lidar_min < SAFE_STOP_DISTANCE:
-            if self.turtlebot_moving == True:
-                self.cmd.linear.x = 0.0 
-                self.cmd.angular.z = 0.0 
-                self.publisher_.publish(self.cmd)
-                self.turtlebot_moving = False
-                self.get_logger().info('Stopping')
-                return
+            self.cmd.linear.x = 0.0
+            self.cmd.angular.z = 0.0
+            self.publisher_.publish(self.cmd)
+            self.get_logger().info('Stopping')
+            self.turtlebot_moving = False
         elif front_lidar_min < LIDAR_AVOID_DISTANCE:
-                self.cmd.linear.x = 0.07 
-                if (right_lidar_min > left_lidar_min):
-                   self.cmd.angular.z = -0.3
-                else:
-                   self.cmd.angular.z = 0.3
-                self.publisher_.publish(self.cmd)
-                self.get_logger().info('Turning')
-                self.turtlebot_moving = True
+            self.cmd.linear.x = 0.07
+            if right_lidar_min > left_lidar_min:
+                self.cmd.angular.z = -0.3
+            else:
+                self.cmd.angular.z = 0.3
+            self.publisher_.publish(self.cmd)
+            self.get_logger().info('Turning')
+            self.turtlebot_moving = True
         else:
             self.cmd.linear.x = 0.3
-            self.cmd.linear.z = 0.0
+            self.cmd.angular.z = 0.0
             self.publisher_.publish(self.cmd)
             self.turtlebot_moving = True
-            
 
-        self.get_logger().info('Distance of the obstacle : %f' % front_lidar_min)
-        self.get_logger().info('I receive: "%s"' %
-                               str(self.odom_data))
-        if self.stall == True:
-           self.get_logger().info('Stall reported')
-        
-        # Display the message on the console
-        self.get_logger().info('Publishing: "%s"' % self.cmd)
- 
-
+        self.get_logger().info('Distance of the obstacle: %f' % front_lidar_min)
 
 def main(args=None):
-    # initialize the ROS communication
     rclpy.init(args=args)
-    # declare the node constructor
     random_walk_node = RandomWalk()
-    # pause the program execution, waits for a request to kill the node (ctrl+c)
     rclpy.spin(random_walk_node)
-    # Explicity destroy the node
     random_walk_node.destroy_node()
-    # shutdown the ROS communication
     rclpy.shutdown()
 
-
-
-if __name__ == '__main__':
-    main()
+if _name_ == '_main_':
+    main()
